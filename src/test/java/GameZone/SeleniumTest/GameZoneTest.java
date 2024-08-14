ca0a06ee13f9daf11c5363aefb4ee349774989c0
Thariq <Thariq93@yahoo.com.sg>
2024-08-14 20:40:33 +0800

    test

--- src/test/java/GameZone/SeleniumTest/GameZoneTest.java
+++ src/test/java/GameZone/SeleniumTest/GameZoneTest.java
@@ -17,21 +17,7 @@ public class GameZoneTest {
       // Load the registration page
       webDriver.navigate().to("http://localhost:8090/GameZone/register.jsp");
       
-      // Fill in the registration form
-      //webDriver.findElement(By.name("Name")).sendKeys("test");
-      //webDriver.findElement(By.name("password")).sendKeys("test");
-      //webDriver.findElement(By.name("email")).sendKeys("test@test.com");
-      //webDriver.findElement(By.name("contact")).sendKeys("12345678");
-      //webDriver.findElement(By.name("dob")).sendKeys("11/11/2011");
-      
-      // Select English from the language dropdown
-      //WebElement languageDropdown = webDriver.findElement(By.name("language"));
-      //languageDropdown.findElement(By.xpath("//option[. = 'English']")).click();
-      
-      // Click the register button
-      //webDriver.findElement(By.name("register")).click();
-      
-      // Verify that the page has navigated to the RegisterServlet
+
       Assert.assertEquals(webDriver.getTitle(), "Register");
       System.out.println("Title : " + webDriver.getTitle());
   }
